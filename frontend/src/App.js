58b3a271852bba089fb082806d64d8a090bc17fb
ChrisSolan <chrissolanopoly@gmail.com>
2024-10-04 15:59:02 -0400

    Add basic homepage
    Typescript configured and setup for the frontend now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Home} from './pages/home.tsx';
+
 function App() {
   return (
     <div className="App">
-      <p>test</p>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,8 @@
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <h1>TEST HOMEPAGE</h1>
+        </div>
+    )
+}
